dad4526b648c0a86c6de4b12a0a07c3f2e59a8ff
Hector Macassa <cbmacassa@gmail.com>
2024-07-24 15:04:15 +0200

    Day 1

---
+++ Day_1/helloworld.py
@@ -0,0 +1,13 @@
+# Day 1 - 30 Days of Python challenge
+
+print(3 + 4) # Addition
+print(3 - 4) # Subtraction
+print(3 * 4) # Multiplication
+print(3 % 4) # Modulus
+print(3 // 4) # Floor Division
+
+print(type(10))
+print(type(9.8))
+print(type(3.14))
+print(type(4 - 4j))
+print(type(['Bobby', 'Python', 'South Africa']))
