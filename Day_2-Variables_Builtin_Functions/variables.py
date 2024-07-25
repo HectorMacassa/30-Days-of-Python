6d07685bfe41e1e2dea52624457af124a7a8d7a5
Hector Macassa <cbmacassa@gmail.com>
2024-07-25 18:38:06 +0200

    Day 2

---
+++ Day_2-Variables_Builtin_Functions/variables.py
@@ -0,0 +1,26 @@
+# Day 2
+
+firstName = "Bobby"
+lastName = "Bacala"
+fullName = "Bobby Bacala"
+country = "United States of America"
+city = "Newark"
+age = 45
+year = 2007
+weight = 265.5
+isMarried = True
+
+print(type(firstName))
+print(type(lastName))
+print(type(fullName))
+print(type(country))
+print(type(city))
+print(type(age))
+print(type(year))
+print(type(weight))
+print(type(isMarried))
+
+print(len(fullName))
+
+num1 = 5
+num2 = 4
