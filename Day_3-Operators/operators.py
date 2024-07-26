a0859fdb06542da69e55259095e4c18cfc8135c3
Hector Macassa <cbmacassa@gmail.com>
2024-07-26 23:12:07 +0200

    Day 3

---
+++ Day_3-Operators/operators.py
@@ -0,0 +1,19 @@
+# Day 3
+
+# Area of Triangle
+base = input("Enter length of base: ")
+height = input("Enter height: ")
+
+area = 0.5 * float(base) * float(height)
+
+print("The area of the triangle is: " + str(area))
+
+# Perimeter of a Triangle
+
+a = int(input("Enter side a: "))
+b = int(input("Enter side b: "))
+c = int(input("Enter side c: "))
+
+p = a + b + c
+
+print("The perimeter of the Triangle is: " + str(p))
